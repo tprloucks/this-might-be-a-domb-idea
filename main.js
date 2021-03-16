e1bcba3a1251f35edcf861dcff539fe44032537a
Michael Loucks <michael.loucks@codeimmersives.com>
2021-03-16 09:50:10 -0500

    hello world

---
+++ main.js
@@ -0,0 +1,26 @@
+const p = document.querySelector('p');
+p.style.color ="lightblue"
+
+const heading = document.querySelector('p');
+heading.style.fontSize = "48px"
+
+const paragraph = document.querySelector('p');
+paragraph.innerHTML = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
+
+const o = document.querySelector("#item-13");
+o.style.opacity = "50%";
+
+const three = document.querySelector('#item-3');
+three.innerHTML = "I say,"+ " Hi!"
+
+const image = document.querySelector('img');
+image.src = "http://www.tioxic.com/wp-content/uploads/trex_4.jpg";
+image.style.height = "300px";
+image.style.width = "auto";
+
+const secondImage = document.querySelector('two')
+secondImage.style.height = "300px"
+
+
+
+
